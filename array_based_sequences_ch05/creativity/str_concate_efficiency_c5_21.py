54cbfce96e5a730f3fbe6de71151af5adbecdc42
richard <wanyikg@163.com>
2017-10-17 20:10:55 +0800

    modify c5.21

--- array_based_sequences_ch05/creativity/str_concate_efficiency_c5_21.py
+++ array_based_sequences_ch05/creativity/str_concate_efficiency_c5_21.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 # This is the exercise of c5.21
+# from the result, I get the conclusion that all the concate method is in the
+# same asyptotic time. I guess my python virtual machine optimize the add
+# concatenation by directly mutating the string object when there is no other
+# reference to the string.
 from timeit import timeit
 
 def add_concate(document):
@@ -33,26 +37,30 @@ def gen_document(dlen):
         temp.append(chr(i % 26 + 65))
     return temp
 
+sp_base = """
+from __main__ import add_concate
+from __main__ import append_concate
+from __main__ import list_comp_concate
+from __main__ import gen_comp_concate
+from __main__ import gen_document
+"""
+
 if __name__ == "__main__":
     tnum = 1
     print("{0:>20s}    {1}    {2}".format("function", "doc_len", "time"))
     try:
-        for i in range(2):
-            doc = gen_document(10**i * 10000)
-            s = "add_concate(" + str(doc) + ")"
-            t_add = timeit(stmt=s, setup="from __main__ import add_concate", number=tnum)
+        for i in range(4):
+            sp = sp_base + "doc = gen_document(" + str(10**i * 10000) + ")"
+            t_add = timeit("add_concate(doc)", setup=sp, number=tnum)
             print("{0:>20s}    10^{1:d}    {2:<.6f}".format("add_concate", i, t_add))
 
-            s = "append_concate(" + str(doc) + ")"
-            t_add = timeit(stmt=s, setup="from __main__ import append_concate", number=tnum)
+            t_add = timeit("append_concate(doc)", setup=sp, number=tnum)
             print("{0:>20s}    10^{1:d}    {2:<.6f}".format("append_concate", i, t_add))
 
-            s = "list_comp_concate(" + str(doc) + ")"
-            t_add = timeit(stmt=s, setup="from __main__ import list_comp_concate", number=tnum)
+            t_add = timeit("list_comp_concate(doc)", setup=sp, number=tnum)
             print("{0:>20s}    10^{1:d}    {2:<.6f}".format("list_comp_concate", i, t_add))
 
-            s = "gen_comp_concate(" + str(doc) + ")"
-            t_add = timeit(stmt=s, setup="from __main__ import gen_comp_concate", number=tnum)
+            t_add = timeit("gen_comp_concate(doc)", setup=sp, number=tnum)
             print("{0:>20s}    10^{1:d}    {2:<.6f}".format("gen_comp_concate", i, t_add))
             print("")
     except KeyboardInterrupt:
