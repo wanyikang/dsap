982151a155732c83bdabab32b224150b0ee53c6b
richard <wanyikg@163.com>
2017-10-18 22:21:12 +0800

    c5.30

---
+++ array_based_sequences_ch05/creativity/assemble_packets_c5_30.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# This is exercise of c5.30
+# Use a list to do this in O(n) time.
+#
+# Initialize a list of length n and all elements is None value, then insert the
+# packets into the list by using the packet number as the index. So construct
+# the list and iterate the packets will take O(n) time.
