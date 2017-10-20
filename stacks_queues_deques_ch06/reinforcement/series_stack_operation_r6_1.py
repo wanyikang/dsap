b996a6825ad390e24444ac719a8b5f3926e911a3
richard <wanyikg@163.com>
2017-10-20 17:03:44 +0800

    r6.1

---
+++ stacks_queues_deques_ch06/reinforcement/series_stack_operation_r6_1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# push(5)
+# push(3)
+# pop()         # return 3
+# push(2)
+# push(8)
+# pop()         # 8
+# pop()         # 2
+# push(9)
+# push(1)
+# pop()         # 1
+# push(7)
+# push(6)
+# pop()         # 6
+# pop()         # 7
+# push(4)
+# pop()         # 4
+# pop()         # 9
+#
+# the stack is like below table:
+#
+#
+#            6-
+#      8- 1- 7- 4-
+#   3- 2- 9-
+#   5
+#
+# finally, there is only integer 5 left in the stack.
