17a16d97a6ea3d125e2be41275c9a937e2fc8752
richard <wanyikg@163.com>
2017-10-20 17:19:51 +0800

    r6.2

---
+++ stacks_queues_deques_ch06/reinforcement/current_size_r6_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# There are only 3 operations that raised Empty errors. And the current size
+# depends on how many pop operations raise error, the result is shown in the
+# following table.
+# top_failed   pop_failed     size
+# 3             0           25-10+0=15
+# 2             1           25-10+1=16
+# 1             2           25-10+2=17
+# 0             3           25-10+3=18
+
