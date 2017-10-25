ab5558b0c789fd24932ce7caf095bc0e7060cf71
richard <wanyikg@163.com>
2017-10-25 15:22:42 +0800

    c6.15

---
+++ stacks_queues_deques_ch06/creativity/max_integer_c6_15.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# x = stack.pop()
+# if x is smaller than stack.pop()
+#     x = stack.top()
+#
+# The above method get the big one of the first two elements from top to bottom
+# in the stack. So we have 2/3 probability to get the max integer in three. So
+# ofcause the big one the first two elements should be assigned to variable x.
+
