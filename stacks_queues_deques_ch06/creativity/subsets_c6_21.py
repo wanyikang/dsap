674e3b5e4410d22ff7a65fb14b99558e1ec0c59c
richard <wanyikg@163.com>
2017-10-30 12:26:46 +0800

    c6.21 recursive version

--- stacks_queues_deques_ch06/creativity/subsets_c6_21.py
+++ stacks_queues_deques_ch06/creativity/subsets_c6_21.py
@@ -2,7 +2,26 @@
 from copy import copy
 from array_stack_c6_16 import ArrayStack
 from queue import MyQueue
-def subsets(l):
+
+def subsets_recursive(l):
+    rlt = []
+    length = len(l)
+    if not l:
+        return []
+    elif length == 1:
+        return [l, []]
+    else:
+        reduced_l = l[1:]
+        rm_elmnt = l[0:1]
+        subs = subsets_recursive(reduced_l)
+        for item in subs:
+            rlt.append(item)
+            tmp = copy(item)
+            tmp.extend(rm_elmnt)
+            rlt.append(tmp)
+        return rlt
+
+def subsets_stack(l):
     rlt = []
     s = ArrayStack()
     q = MyQueue()
@@ -24,7 +43,10 @@ def subsets(l):
     return rlt
 
 if __name__ == '__main__':
-    l = [i for i in range(1, 5)]
-    r = subsets(l)
-    print(r)
+    l = [i for i in range(1, 3)]
+    rs = subsets_stack(l)
+    print("subsets_stack: ", rs)
+    rr = subsets_recursive(l)
+    rr.remove([])
+    print("subsets_recursive: ", rr)
 
