813b0073cd2abd181a1d5d0c33b181345c30fcfd
richard <wanyikg@163.com>
2017-10-30 17:54:57 +0800

    c6.23

---
+++ stacks_queues_deques_ch06/creativity/c6_23.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# 0, get elements number for S and T. Let's say cs for S, ct for T
+# 1, pop all elements from S and push to R. By the way, keep the order, same as
+# the following steps.
+# 2, pop cs elements from R and push to T
+# 3, pop cs+ct elements from T and push to R
+# 4, pop cs+ct elements from R and push to S, done
+
