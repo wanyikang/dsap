67912ae71db26b76c33196b094490db62964ec0e
richard <wanyikg@163.com>
2017-11-27 17:38:19 +0800

    r8.1

---
+++ trees_ch08/reinforcement/question_r8_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# a: /user/rt/courses/ is root
+# b: internal nodes: [cs016/, homeworks/, programs/, cs252/, projects/, papers/,
+#    demos/]
+# c: 10
+# d: 2
+# e: grades, programs/
+# f: [projects/, papers/, demos/, buylow, sellhigh, market]
+# g: 3
+# h: 4
+
