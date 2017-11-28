54bf88399719a9a1a80597c2e4281b666b2c09f1
richard <wanyikg@163.com>
2017-11-28 10:22:31 +0800

    r8.6

---
+++ trees_ch08/reinforcement/represent_inproper_r8_6.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Inproper binary tree T has n nodes, if we want to convert it to a proper
+# tree by filling with dummy nodes, How many nodes do we need at least?
+# A binary node has at most two children, so nodes with two or zero children do
+# not need filling, let f be the number of nodes need filling, then 0<= f <= n.
+# So we need f dummy nodes to convert the inproper binary tree to proper
+# binary tree. So the proper tree has O(n) nodes.
+
