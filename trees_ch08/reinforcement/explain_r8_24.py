23a297c2c7e30bf9d5263f3fc724086303960a02
richard <wanyikg@163.com>
2017-11-30 16:58:37 +0800

    r8.24

---
+++ trees_ch08/reinforcement/explain_r8_24.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# A proper tree with more than one node, the preorder traveral can not be same
+# order with postorder traversal, and also preorder traversal can not be
+# reverse order with postorder traversal too. It obviously.
+
