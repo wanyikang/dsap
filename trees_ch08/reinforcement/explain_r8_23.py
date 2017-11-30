43a45879bf5ff09534b046429fb4d5251febf491
richard <wanyikg@163.com>
2017-11-30 16:23:34 +0800

    r8.23

---
+++ trees_ch08/reinforcement/explain_r8_23.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# A tree with more than one node, the preorder traveral can not be same order
+# with postorder traversal, It's obviously. But preorder traversal may be
+# reverse order with postorder traversal. For example, a tree with only two
+# nodes.
+
