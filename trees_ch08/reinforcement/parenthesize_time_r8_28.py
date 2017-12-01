67286cdf4138176f0d54e06e38a9f17047249047
richard <wanyikg@163.com>
2017-12-01 11:44:52 +0800

    r8.28

---
+++ trees_ch08/reinforcement/parenthesize_time_r8_28.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# It's just like _height2 method for a general tree. the totle time is amount
+# to every node perform the parenthesize code (except for the time running
+# children's parenthesize method), and all the other python sentences is
+# running in constant time. So the running time is O(n).
+
