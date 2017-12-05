103f222275fe28399d72fc92508eced5fc3180b6
richard <wanyikg@163.com>
2017-12-05 10:24:40 +0800

    modify c8.32

--- trees_ch08/creativity/justify_summary_of_depth_c8_32.py
+++ trees_ch08/creativity/justify_summary_of_depth_c8_32.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# This exercise is wrong, becasue we can get a tree that the sum of external
+# nodes depth is Ω(n^2), see c8.33 for detail. Let's add the maximum number of
+# external nodes situation another condition: the tree is banlance.
+#
 # Like reinforcement 8.7. The minimum external nodes is 1, The maximum external
 # nodes is a proper tree, that's (n+1)/2.
 # minimum: D = n-1, is O(n).
