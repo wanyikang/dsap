8ff70b942b501965a1a476604600a04e5c503fce
richard <wanyikg@163.com>
2017-12-11 11:30:52 +0800

    c8.43

---
+++ trees_ch08/creativity/traversal_c8_43.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# (a
+# preorder T:  ABEFCDG
+# preorder T': ABEFCDG
+# So the answer is yes
+#
+# (b
+# postorder T:  EFBCGDA
+# postorder T': FEGDCBA
+# So the answer is no
+#
+# (c
+# inorder T': EFBCGDA
+# So the answer is postorder of T is equal to inorder of T'
+
