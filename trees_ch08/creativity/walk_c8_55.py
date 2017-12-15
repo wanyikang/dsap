3a2f9e29d5fae73ca489ac7f56d8ec02ec773896
richard <wanyikg@163.com>
2017-12-15 17:23:55 +0800

    c8.55

---
+++ trees_ch08/creativity/walk_c8_55.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# When the topdown parameter is True or ignored, the walk method performs a
+# preorder traversal, when the topdown parameter is False, the walk method
+# performs a postorder traversal.
+
