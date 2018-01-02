b35c53225f531463c5875a6f5bd4574589fe2de7
richard <wanyikg@163.com>
2018-01-02 20:40:20 +0800

    r9.10

---
+++ priority_queue_ch09/reinforcement/third_smallest_position_r9_10.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# The second smallest element can be the left child of root element or the
+# right child of root element.
+#
+# 1, second smallest element is left child of root:
+#   then, the third smallest element may be one of (left child of second
+#   smallest, right child of second smallest, right child of root)
+#
+# 2, second smallest element is right child of root:
+#   then, the third smallest element may be one of (left child of second
+#   smallest, right child of second smallest, left child of root)
+
