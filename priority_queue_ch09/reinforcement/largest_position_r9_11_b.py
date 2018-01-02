0b81667d7a44566c2498e6546d72a132833e3967
richard <wanyikg@163.com>
2018-01-02 21:16:06 +0800

    r9.11

---
+++ priority_queue_ch09/reinforcement/largest_position_r9_11_b.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# See picture largest_position_r9_11_a.jpeg, we can not tell which position is
+# the bigest. Without comparation, all we can get is that the bigest element
+# is one of the leaf position.
+
