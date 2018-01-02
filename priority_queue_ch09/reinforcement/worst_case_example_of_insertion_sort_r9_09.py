36b8854d6186bba3d22ac9fd1d6d4636fbe3e150
richard <wanyikg@163.com>
2018-01-02 20:26:30 +0800

    r9.09

---
+++ priority_queue_ch09/reinforcement/worst_case_example_of_insertion_sort_r9_09.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# For example: (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+# Recall the exercise r9.8, we get the running time of the aboving sequence:
+# (1+2+...+12), means (1+n)*n/2, where n=12
+# So, the running time of the aboving sequence is Ω(n^2)
+
