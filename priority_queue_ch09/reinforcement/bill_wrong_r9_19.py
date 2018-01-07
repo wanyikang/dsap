dc5e6c7eab1f3332467d9e808a29e21cf355548f
richard <wanyikg@163.com>
2018-01-07 18:37:01 +0800

    r9.19

---
+++ priority_queue_ch09/reinforcement/bill_wrong_r9_19.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# The preorder traversal of the following tree is 031, so Bill is wrong.
+#
+#         (0, a)
+#           / \
+#          /   \
+#         /     \
+#      (3, b)  (1, c)
+
