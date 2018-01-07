3e52bf980f1b7ccf747bf46a9ef352f3856066b1
richard <wanyikg@163.com>
2018-01-07 18:38:45 +0800

    r9.20

---
+++ priority_queue_ch09/reinforcement/hillary_wrong_r9_20.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# The preorder traversal of the following tree is 130, so Hillary is wrong.
+#
+#         (0, a)
+#           / \
+#          /   \
+#         /     \
+#      (1, b)  (3, c)
+
