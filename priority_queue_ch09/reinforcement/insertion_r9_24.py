bdb5fbda0c0aa2d1714ad2c2fa539949f8c5be16
richard <wanyikg@163.com>
2018-01-08 14:44:45 +0800

    r9.24

---
+++ priority_queue_ch09/reinforcement/insertion_r9_24.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Describe a sequence of n insertions in a heap that requires Ω(nlogn) time to
+# process.
+#
+# That means every insertion needs Ω(logn) time, so that all n sequence need
+# Ω(nlogn) time. And That means every insertion is the minimum element in the
+# heap. So we can list a sequence like the following:
+# [5,4,3,2,1,0]
+
