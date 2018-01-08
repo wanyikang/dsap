694c554638d9b4a43b99f01b56429608cabda8d6
richard <wanyikg@163.com>
2018-01-08 22:51:29 +0800

    modify c9.32

--- priority_queue_ch09/creativity/search_last_node_c9_32.py
+++ priority_queue_ch09/creativity/search_last_node_c9_32.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+# Let last be the reference of last node. p be the reference of parent node of
+# last. Do simple up-and-down searches in the tree to locate the last node each
+# time.
+#
+# For add, if right child of p is last, then make p be last, and do the
+# aboving step again untile p is root or p.right() is not last, then let last
+# be the sibling of last, and get the left of last until a leaf.
+#
+# For remove_min, the procedure is very similar, following is the algorithm:
 # Algorithm search_last_node(t, walk, up=True):
 #   if len(t) == 0 or len(t) == 1:
 #       return None
