34239d6a438854ba014166bbc20e9c4489d62bda
richard <wanyikg@163.com>
2018-01-12 10:04:12 +0800

    c9.42

---
+++ priority_queue_ch09/creativity/search_largest_k_elements_c9_42.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# First, using a bottom-up construction method to build the maximum-oriented
+# heap, that takes O(n) time.
+# Second, remove max element k times, that takes k*logn time, so the total time
+# is O(n + k*logn).
+
