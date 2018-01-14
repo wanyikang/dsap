7d2a2ca640ccf64c17cf7d71a7fdb75cb086a8dc
richard <wanyikg@163.com>
2018-01-14 17:56:52 +0800

    c9.49

---
+++ priority_queue_ch09/creativity/in_palce_min_heap_sort_c9_49.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 1, construct a min-oriented heap in O(n) time.
+# 2, remove min and palce the element in the end of list, and we get a
+# non-increasing list. It takes O(n*logn) time.
+# 3, find how many elements will be swaped near the middle of the list, and
+# swap all of them in O(n/2) time.
+
