c4c6cd895f8a49e8b2cbf9ffb90206cac59f72bd
richard <wanyikg@163.com>
2018-02-27 21:14:57 +0800

    r10.02

---
+++ maps_hashtables_skiplists_ch10/reinforcement/items_r10_02.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# If directly applied to the UnsortedTableMap, the running time is O(n^2).
+import sys
+sys.path.append('..')
+from map_base import MapBase
+
+class MyMap(MapBase):
+    """ For give a concrete implementation of items method."""
+
+    def items(self):
+        """ Return a set-like view of (k,v) tuples for all entries."""
+        for k in self:
+            yield (k, self[k])
+
--- maps_hashtables_skiplists_ch10/reinforcement/pop_r10_01.py
+++ maps_hashtables_skiplists_ch10/reinforcement/pop_r10_01.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
-
 import sys
 sys.path.append('..')
 from map_base import MapBase
 
 class MyMap(MapBase):
-    """ For gvie a concrete implementation of pop method."""
+    """ For give a concrete implementation of pop method."""
 
     def pop(self, k, d=None):
         """ Remove the item associated with key k from the map and return its
