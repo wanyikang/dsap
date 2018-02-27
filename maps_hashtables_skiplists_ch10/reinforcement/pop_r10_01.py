3c9df90ccbc1ae81a8d0623b509561e8ae5f9f80
richard <wanyikg@163.com>
2018-02-27 18:09:49 +0800

    r10.1

---
+++ maps_hashtables_skiplists_ch10/reinforcement/pop_r10_01.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append('..')
+from map_base import MapBase
+
+class MyMap(MapBase):
+    """ For gvie a concrete implementation of pop method."""
+
+    def pop(self, k, d=None):
+        """ Remove the item associated with key k from the map and return its
+        associated value. If k is not in the map, return the default value d (
+        or raise KeyError if d is None).
+        """
+        try:
+            ret = self[k]
+        except KeyError as kerr:
+            if d is not None:
+                return d
+            else:
+                raise kerr
+        del self[k]
+        return ret
+
