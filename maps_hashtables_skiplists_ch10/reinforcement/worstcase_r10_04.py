0b9761fd2776736bb90202e8b0fc72fb0efd114c
richard <wanyikg@163.com>
2018-02-28 21:53:44 +0800

    r10.4

---
+++ maps_hashtables_skiplists_ch10/reinforcement/worstcase_r10_04.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# The first insertion takes O(1) time, the second takes O(2) time...
+# and the nth insertion takes O(n) time, so the totle time is :
+# O(1 + 2 + 3 + ... + n) = O(n^2)
+
