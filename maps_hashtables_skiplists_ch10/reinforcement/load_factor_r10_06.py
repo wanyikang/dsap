fc6bdd21e5587e0015975ed2d988da270e34e502
richard <wanyikg@163.com>
2018-02-28 22:39:50 +0800

    r10.6

---
+++ maps_hashtables_skiplists_ch10/reinforcement/load_factor_r10_06.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# seperate chaining    > 1    suggest < 0.9
+# linear probing       < 1    suggest < 0.5
+# quadratic probing    < 1    suggest < (number that a bit higher than 0.5)
+# double hash          < 1    suggest < (number that a bit higher than 0.5)
+# pseudo random        < 1    suggest < (number that a bit higher than 0.5)
+
