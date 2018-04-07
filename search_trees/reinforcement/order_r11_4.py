7a7e3b704b6a513d782df10e34b8b766228b3282
richard <wanyikg@163.com>
2018-04-07 22:12:57 +0800

    r11.4

---
+++ search_trees/reinforcement/order_r11_4.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# I think the answer of the previous question can also resolve this problem
+# too. For example, the tree corresponding set (1,2,3) and that
+# corresponding set (3,2,1) is different.
+
